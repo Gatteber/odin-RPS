3ded93b101b602dee9580b8877f79a90a91ec1f9
Gatteber <Garrett.Atteberry@gmail.com>
2022-06-02 10:06:13 +0900

    Finish playRound function, begin work on player input.

--- javascript.js
+++ javascript.js
@@ -9,26 +9,42 @@ function computerPlay () {
         let cpuActual = getCpuInput(1, 4);
         //console.log(cpuActual);
         if (cpuActual === 1) {
-            console.log("rock");
+        //    console.log("rock");
             return "rock";
         } else if (cpuActual === 2) {
-            console.log("paper");
+        //    console.log("paper");
             return "paper";
         } else {
-            console.log("scissors");
+        //    console.log("scissors");
             return "scissors";
         }
         }
+//Comparison operation
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "tie"
+    } else if (playerSelection.startsWith("r") && computerSelection.startsWith("s")) {
+        return "winner"
+    } else if (playerSelection.startsWith("p") && computerSelection.startsWith("r")) {
+    return "winner"
+    }  else if (playerSelection.startsWith("s") && computerSelection.startsWith("p")) {
+    return "winner"
+    }  else {
+    return "loser"
+    }
+
+}
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "tie"
-    } else {
-        return "error catch"
-    }
-
-}
 console.log(playRound(playerSelection, computerSelection));
+
+let value = prompt("Lets play Rock Paper Scissors! Enter one of the values!");
+
+ if (value == null) {
+     alert("error, exited");
+ }
+if (value.toLowerCase() == "rock") {
+    alert("You entered ROCK!");
+}
