bf8273a96d9698eb9c04286d06e3bb8495eb38ac
Gatteber <Garrett.Atteberry@gmail.com>
2022-06-02 10:46:54 +0900

    add game function and player input, button doesn't work, need to fix.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-//Notes
-
 //Fetching random CPU input
 function getCpuInput (max, min) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -34,17 +32,45 @@ function playRound (playerSelection, computerSelection) {
     }
 
 }
+//GAME TEST (unused, please don't uncomment)
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
 
-console.log(playRound(playerSelection, computerSelection));
 
-let value = prompt("Lets play Rock Paper Scissors! Enter one of the values!");
+//GUESSING HOW TO PROMPT USER (unused, please don't uncomment)
+// let value = prompt("Lets play Rock Paper Scissors! Enter one of the values!");
 
- if (value == null) {
-     alert("error, exited");
- }
-if (value.toLowerCase() == "rock") {
-    alert("You entered ROCK!");
+//  if (value == null) {
+//      alert("error, exited");
+//  }
+// if (value.toLowerCase() == "rock") {
+//     alert("You entered ROCK!");
+// }
+
+//game function
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = prompt("Lets play Rock, Paper, Scissors! Enter Rock, Paper or Scissors to begin!")
+        if (playerSelection == null) {
+            console.log("error");
+            alert("error, exited");
+        } else if (playerSelection.toLowerCase() == "rock") {
+            console.log(playRound(playerSelection, computerSelection));
+        } else if (playerSelection.toLowerCase() == "paper") {
+            console.log(playRound(playerSelection, computerSelection));
+        } else if (playerSelection.toLowerCase() == "scissors") {
+            console.log(playRound(playerSelection, computerSelection));
+        } else {
+            console.log("error");
+            alert("error, you didn't follow instructions! You lose this round!")
+        }
+    }
+}
+//Tried to add a button to start the game, couldn't get it to work
+window.onload=function(){
+    const button = document.getElementById("button");
+    button.addEventListener("click", game());
 }
