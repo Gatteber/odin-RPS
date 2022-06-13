405c2e25d0fe573ea659a6744cb20e1c6bce8dac
Gatteber <Garrett.Atteberry@gmail.com>
2022-06-13 19:34:19 +0900

    added event listeners and buttons, have working gameplay

--- README.md
+++ README.md
@@ -41,3 +41,28 @@ Compare, return, done.
 
 ### REFLECTION ###
 Since I have experience with python, this project was relatively straightforward, just learning the differences between variables and running unoptimized code was interesting. I'm disappointed that I couldn't figure out why the addEventListener function wouldn't work, but I was spending too much time on it and therefore went with the simpler (although not as elegant) solution.
+
+
+### Part 2 - DOM Manipulation ###
+Create 3 Buttons, Rock, Paper, and Scissors. //done
+add an event that calls playround function
+
+add a div for displaying results //done
+display the running score and announce the winner of the game at 5 points
+
+
+1.)Start with a page that says
+"Can you outsmart me in Rock, Paper, Scissors?"
+-> maybe some css that transitions between can you.. and ..rock
+
+2.)Create a button that says "Start game", when it does, delete the button, and create a flexbox with 3 buttons //done
+
+3.) add a running tally that keeps track, maybe a reset function?
+
+game is started
+player selects a button
+comparison
+determine winner, update score
+...
+after 5 rounds, if comparison for winner or loser
+display winner/loser.
--- javascript.js
+++ javascript.js
@@ -1,22 +1,97 @@
+//Begin game
+const gameStart = document.querySelector('.start-btn');
+gameStart.addEventListener("click", beginGame, { once: true });
+
+
+function beginGame () {
+    const container = document.querySelector('.rps-box');
+    const rockBtn = document.createElement('button');
+    rockBtn.textContent = "Rock";
+    rockBtn.classList.add('rps-btn');
+    rockBtn.classList.add('rps-rock');
+    container.appendChild(rockBtn); //rock
+
+    const paperBtn = document.createElement('button');
+    paperBtn.textContent = "Paper";
+    paperBtn.classList.add('rps-btn');
+    paperBtn.classList.add('rps-paper');
+    container.appendChild(paperBtn); //paper
+
+    const scissorsBtn = document.createElement('button');
+    scissorsBtn.textContent = "Scissors";
+    scissorsBtn.classList.add('rps-btn');
+    scissorsBtn.classList.add('rps-scissors');
+    container.appendChild(scissorsBtn); //scissors
+
+    const scoreContainer = document.querySelector('.score-box');
+    const scoreBoxPlayer = document.createElement('div');
+    const scoreBoxCpu = document.createElement('div');
+    document.body.appendChild(scoreBoxPlayer);
+    document.body.appendChild(scoreBoxCpu);
+    scoreBoxPlayer.textContent = "Player Score: 0";
+    scoreBoxPlayer.classList.add ('score-child');
+    scoreBoxCpu.textContent = "CPU Score: 0";
+    scoreBoxCpu.classList.add ('score-child'); //score box
+    scoreContainer.appendChild(scoreBoxPlayer);
+    scoreContainer.appendChild(scoreBoxCpu);
+
+    const removeStart = document.querySelector('.start-btn');
+    removeStart.parentNode.removeChild(removeStart);//remove start button
+    console.log("Game prepared");
+
+    //play
+    const playerRock = document.querySelector('.rps-rock');
+    playerRock.addEventListener("click", () => {
+        playerSelection = "r";
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        //scoreBoxPlayer.innerHTML = "1";
+    });
+    const playerPaper = document.querySelector('.rps-paper');
+    playerPaper.addEventListener("click", () => {
+        playerSelection = "p";
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection,computerSelection));
+    });
+        const playerScissors = document.querySelector('.rps-scissors');
+    playerScissors.addEventListener("click", () => {
+        playerSelection = "s";
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    });
+    }
+//     game();
+//     function game () {
+//         for (let i = 0; i <5; i++) {
+//             let computerSelection = computerPlay();
+//             let playerSelection = choose();
+//             console.log(playRound(playerSelection, computerSelection));
+//         }
+//     };
+
+// }
+
+
+
 //Fetching random CPU input
 function getCpuInput (max, min) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 //Take input and convert to RPS
 function computerPlay () {
-        let cpuActual = getCpuInput(1, 4);
-        //console.log(cpuActual);
-        if (cpuActual === 1) {
-        //    console.log("rock");
-            return "rock";
-        } else if (cpuActual === 2) {
-        //    console.log("paper");
-            return "paper";
-        } else {
-        //    console.log("scissors");
-            return "scissors";
-        }
-        }
+    let cpuActual = getCpuInput(1, 4);
+    //console.log(cpuActual);
+    if (cpuActual === 1) {
+    //    console.log("rock");
+    return "rock";
+    } else if (cpuActual === 2) {
+    //    console.log("paper");
+        return "paper";
+    } else {
+    //    console.log("scissors");
+        return "scissors";
+    }
+}
 //Comparison operation
 function playRound (playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
@@ -32,45 +107,29 @@ function playRound (playerSelection, computerSelection) {
     }
 
 }
-//GAME TEST (unused, please don't uncomment)
-// const playerSelection = "rock";
-// const computerSelection = computerPlay();
 
-// console.log(playRound(playerSelection, computerSelection));
+// function playerConversion () {
+//     if ()
 
-
-//GUESSING HOW TO PROMPT USER (unused, please don't uncomment)
-// let value = prompt("Lets play Rock Paper Scissors! Enter one of the values!");
-
-//  if (value == null) {
-//      alert("error, exited");
-//  }
-// if (value.toLowerCase() == "rock") {
-//     alert("You entered ROCK!");
 // }
 
-//game function
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("Lets play Rock, Paper, Scissors! Enter Rock, Paper or Scissors to begin!")
-        if (playerSelection == null) {
-            console.log("error");
-            alert("error, exited");
-        } else if (playerSelection.toLowerCase() == "rock") {
-            console.log(playRound(playerSelection, computerSelection));
-        } else if (playerSelection.toLowerCase() == "paper") {
-            console.log(playRound(playerSelection, computerSelection));
-        } else if (playerSelection.toLowerCase() == "scissors") {
-            console.log(playRound(playerSelection, computerSelection));
-        } else {
-            console.log("error");
-            alert("error, you didn't follow instructions! You lose this round!")
-        }
-    }
-}
-//Tried to add a button to start the game, couldn't get it to work
-// window.onload=function(){
-//     const button = document.getElementById("button");
-//     button.addEventListener("click", game());
-// }
+// function game () {
+//     //add listeners for each button
+//     const playerRock = document.querySelector('.rock-btn');
+//     playerRock.addEventListener("click", function(e) {
+//         console.log(e);
+//     }) 
+
+//     };
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     console.log("DOM Loaded");
+//     const playerRock = document.querySelector('.rps-rock');
+//     playerRock.addEventListener("click", (event) => {
+//         console.log("clicked rock!");
+//     } )
+// })
+
+
+
+
+
